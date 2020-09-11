c126be9ab102457e52b2c169849668628551696a
Axmouth <aggronfg@yahoo.com>
2020-09-11 22:32:07 +0300

    derp

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,4 @@
 touch .env
-docker build . --file asp.net-core-mvc-drink-shop/Dockerfile --tag axmouth/asp-drinks-mvc
+docker build . --file asp.net-core-mvc-drink-shop/Dockerfile --tag ghcr.io/axmouth/asp-drinks-mvc
 docker-compose build
 docker-compose up -d --remove-orphans
